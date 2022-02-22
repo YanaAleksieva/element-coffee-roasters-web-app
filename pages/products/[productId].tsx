6096bb7326145ce1026778d2a4579e6b4914a639
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-22 11:17:25 +0200

    initial pages added

---
+++ pages/products/[productId].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const ProductDetailPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Product Detail</h1>
+    </div>
+  );
+};
+
+export default ProductDetailPage;
---
+++ pages/products/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const AllProductsPage: NextPage = () => {
+  return (
+    <div>
+      <h1>All Products</h1>
+    </div>
+  );
+};
+
+export default AllProductsPage;
