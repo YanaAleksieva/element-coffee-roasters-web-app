e21fc6b3720c650038e4079cc05e9f23e8e2ea01
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-22 17:29:08 +0200

    added logo

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
 
 import classes from "./main-header.module.css";
 
@@ -7,7 +8,17 @@ const MainHeader: NextPage = () => {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href="/">Logo</Link>
+        <Link href="/" passHref>
+          <a>
+            <Image
+              src="/images/logo.png"
+              alt="Element Coffee Roasters Logo"
+              width="160"
+              height="160"
+              priority
+            />
+          </a>
+        </Link>
       </div>
       <nav className={classes.navigation}>
         <ul>
