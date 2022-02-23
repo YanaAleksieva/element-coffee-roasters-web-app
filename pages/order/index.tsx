9ab10f171bb0e6971243568547516501b54de02a
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-23 14:42:53 +0200

    add page for order

---
+++ pages/order/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const OrderPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Order Politics</h1>
+    </div>
+  );
+};
+
+export default OrderPage;
