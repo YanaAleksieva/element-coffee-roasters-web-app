d9046e7ce8f8298e6065165f32113d8a2bee43ba
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-24 19:07:08 +0200

    added product spec to product list page

---
+++ components/products/product-spec.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+import classes from "./product-spec.module.css";
+
+const ProductSpec = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.iconRow}>
+        <div className={classes.iconItem}>
+          <div className={classes.icon}>
+            <Image
+              src="/images/single-origin-white.png"
+              alt="Sinle Origin Coffees"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <p className={classes.specText}>SINGLE ORIGIN COFFEE</p>
+        </div>
+        <div className={classes.iconItem}>
+          <div className={classes.icon}>
+            <Image
+              src="/images/roaster-icon-white-less.png"
+              alt="Freshly Roasted in Stara Zagora"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <p className={classes.specText}>ПРЯСНО ИЗПЕЧЕНО В СТАРА ЗАГОРА</p>
+        </div>
+        <div className={classes.iconItem}>
+          <div className={classes.icon}>
+            <Image
+              src="/images/fast-delivery-bg-white.png"
+              alt="Delivery in Bularia"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <p className={classes.specText}>ДОСТАВКА В ЦЯЛА БЪЛГАРИЯ</p>
+        </div>
+        <div className={classes.iconItem}>
+          <div className={classes.icon}>
+            <Image
+              src="/images/package-white.png"
+              alt="Packages in customer's wanted quantity"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <p className={classes.specText}>РАЗЛИЧНИ ГРАМАЖИ ПАКЕТИ</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductSpec;
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import { getAllProducts } from "../../dummy-products";
 import ProductList from "../../components/products/product-list";
+import ProductSpec from "../../components/products/product-spec";
 
 const AllProductsPage: NextPage = () => {
   const products= getAllProducts();
 
   return (
     <div>
+      <ProductSpec />
       <ProductList items={products} />
     </div>
   );
