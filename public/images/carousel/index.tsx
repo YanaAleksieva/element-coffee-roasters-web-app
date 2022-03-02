68a2f86a9b24564867b9fd9902d47b8007f0a80f
YanaAleksieva <ya.aleksieva@gmail.com>
2022-03-02 11:50:35 +0200

    modify images

--- public/images/carousel/index.tsx
+++ public/images/carousel/index.tsx
@@ -1,8 +1,8 @@
-import media1 from "./media-1.png";
+import media1 from "./media-1.jpg";
 import media2 from "./media-2.jpg";
 import media3 from "./media-3.jpg";
 import media4 from "./media-4.jpg";
-import media5 from "./media-5.jpg";
+import media5 from "./media-5.png";
 import media6 from "./media-6.jpg";
 import media7 from "./media-7.jpg";
 
