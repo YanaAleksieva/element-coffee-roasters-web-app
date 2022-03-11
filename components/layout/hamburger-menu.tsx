89ca10bcf2720d7e0159c6baadd38c735715fe3f
YanaAleksieva <ya.aleksieva@gmail.com>
2022-03-11 16:07:59 +0200

    added icon for hamburger menu

---
+++ components/layout/hamburger-menu.tsx
@@ -0,0 +1,37 @@
+import classes from "./hamburger-menu.module.css";
+
+type Hamburger = {
+  isOpen: boolean;
+};
+
+const HamburgerMenu = (props: Hamburger) => {
+  const { isOpen } = props;
+
+  return (
+    <div className={classes.hamburger}>
+      <div
+        className={`${
+          isOpen
+            ? `${classes.burger} ${classes.burger1Rotate}`
+            : `${classes.burger} ${classes.burger1}`
+        }`}
+      />
+      <div
+        className={`${
+          isOpen
+            ? `${classes.burger} ${classes.burger2Rotate}`
+            : `${classes.burger} ${classes.burger2}`
+        }`}
+      />
+      <div
+        className={`${
+          isOpen
+            ? `${classes.burger} ${classes.burger3Rotate}`
+            : `${classes.burger} ${classes.burger3}`
+        }`}
+      />
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 import classes from "./main-header.module.css";
 import { useRouter } from "next/router";
+import HamburgerMenu from "./hamburger-menu";
 
 const MainHeader: NextPage = () => {
   const router = useRouter();
+
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburgerHandler = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
 
   return (
     <header className={classes.header}>
@@ -43,6 +51,9 @@ const MainHeader: NextPage = () => {
             <Link href="/order">ПОРЪЧАЙ</Link>
           </li>
         </ul>
+        <div className={classes.hamburger} onClick={toggleHamburgerHandler}>
+          <HamburgerMenu isOpen={hamburgerOpen} />
+        </div>
       </nav>
     </header>
   );
