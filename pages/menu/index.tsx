5977fc3d8f9aa6cbbc193f504fbced4ea09accd9
YanaAleksieva <ya.aleksieva@gmail.com>
2022-03-23 17:34:14 +0200

    added framer-motion and animation to menu in

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,8 +1,27 @@
 import type { NextPage } from "next";
 import MenuList from "../../components/layout/menu-list";
+import { motion, AnimatePresence } from "framer-motion";
 
 const MenuPage: NextPage = () => {
-  return <MenuList />;
+  const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    //exit: { opacity: 0, x: 200, y: 0 },
+  };
+  const varTest = Math.random();
+
+  return (
+    <motion.div
+      key={`${`modal${varTest}`}`}
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      //exit="exit"
+      transition={{ type: "linear" }} // Set the transition to linear
+    >
+      <MenuList />
+    </motion.div>
+  );
 };
 
 export default MenuPage;
