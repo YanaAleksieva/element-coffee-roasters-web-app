b643a0a072eda17e9e6ff09cbc6f30882603c9b5
YanaAleksieva <ya.aleksieva@gmail.com>
2022-03-23 11:15:18 +0200

    basic menu page added

---
+++ components/layout/menu-list.tsx
@@ -0,0 +1,8 @@
+import HeaderLinks from "./header-links";
+import classes from "./menu-list.module.css";
+
+const MenuList = () => {
+  return <section className={classes.main}><HeaderLinks /></section>;
+};
+
+export default MenuList;
---
+++ pages/menu/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import MenuList from "../../components/layout/menu-list";
+
+const MenuPage: NextPage = () => {
+  return <MenuList />;
+};
+
+export default MenuPage;
