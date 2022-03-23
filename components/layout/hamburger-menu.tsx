82cd1e7e64b4f0cc30c628d69ba217b6c816eabe
YanaAleksieva <ya.aleksieva@gmail.com>
2022-03-23 11:49:29 +0200

    when X clicked redirect to previous page

--- components/layout/hamburger-menu.tsx
+++ components/layout/hamburger-menu.tsx
@@ -1,5 +1,6 @@
 import classes from "./hamburger-menu.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 type Hamburger = {
   isOpen: boolean;
@@ -7,34 +8,51 @@ type Hamburger = {
 
 const HamburgerMenu = (props: Hamburger) => {
   const { isOpen } = props;
+  const router = useRouter();
 
-  return (
-    <Link href="/menu" passHref>
-      <div className={classes.hamburger}>
-        <div
-          className={`${
-            isOpen
-              ? `${classes.burger} ${classes.burger1Rotate}`
-              : `${classes.burger} ${classes.burgerNoRotate}`
-          }`}
-        />
-        <div
-          className={`${
-            isOpen
-              ? `${classes.burger} ${classes.burger2Rotate}`
-              : `${classes.burger} ${classes.burger2}`
-          }`}
-        />
-        <div
-          className={`${
-            isOpen
-              ? `${classes.burger} ${classes.burger3Rotate}`
-              : `${classes.burger} ${classes.burgerNoRotate}`
-          }`}
-        />
-      </div>
-    </Link>
+  const hamMenu = (
+    <div className={classes.hamburger}>
+      <div
+        className={`${
+          isOpen
+            ? `${classes.burger} ${classes.burger1Rotate}`
+            : `${classes.burger} ${classes.burgerNoRotate}`
+        }`}
+      />
+      <div
+        className={`${
+          isOpen
+            ? `${classes.burger} ${classes.burger2Rotate}`
+            : `${classes.burger} ${classes.burger2}`
+        }`}
+      />
+      <div
+        className={`${
+          isOpen
+            ? `${classes.burger} ${classes.burger3Rotate}`
+            : `${classes.burger} ${classes.burgerNoRotate}`
+        }`}
+      />
+    </div>
   );
+
+  if (!isOpen) {
+    return (
+      <Link href="/menu" passHref>
+        {hamMenu}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        className={classes.btn}
+        type="button"
+        onClick={() => router.back()}
+      >
+        {hamMenu}
+      </button>
+    );
+  }
 };
 
 export default HamburgerMenu;
