afbd2d8df6bc3d98175eae3541878c42212cba76
YanaAleksieva <ya.aleksieva@gmail.com>
2022-05-19 11:10:10 +0300

    added basic description component and order page content

---
+++ components/layout/description-page.tsx
@@ -0,0 +1,27 @@
+import classes from "./description-page.module.css";
+import Image from "next/image";
+
+type DescriptionProps = {
+  children: React.ReactNode;
+  imgUrl: string;
+  imgAlt: string;
+};
+
+const DescriptionPage = (props: DescriptionProps) => {
+  return (
+    <div className={classes.container}>
+      <p className={classes.text}>{props.children}</p>
+      <div className={classes.image}>
+        <Image
+          src={props.imgUrl}
+          alt={props.imgAlt}
+          layout="fill"
+          objectFit="scale-down"
+          priority
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionPage;
--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
+import { Fragment } from "react";
+import DescriptionPage from "../../components/layout/description-page";
 
 const OrderPage: NextPage = () => {
   return (
-    <div>
-      <h1>Order Politics</h1>
-    </div>
+    <Fragment>
+      <DescriptionPage imgUrl={"/images/cover-wider.png"} imgAlt={"coffee-shop-photo"}>
+        За да поръчате, моля обадете се на посочения номер в секцията ЗА НАС,
+        пишете ни съобщение на Facebook страницата или заповядайте лично в нашия
+        магазин - гр. Стара Загора, ул. Димитър Наумов 70.
+      </DescriptionPage>
+    </Fragment>
   );
 };
 
